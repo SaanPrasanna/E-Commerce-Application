8186041966f4db756ed0efe94a77d80325d3c269
Sandun Prasanna <sandunpmapa@gmail.com>
2025-12-02 22:26:51 +0530

    :zap: Add DTO's to Application

---
+++ E-CommerceApplication.Core/DTOs/Order/OrderDtos.cs
@@ -0,0 +1,63 @@
+﻿using E_CommerceApplication.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceApplication.Core.DTOs.Order {
+    public class CreateOrderDto {
+        public Guid ShippingAddressId { get; set; }
+        public string? Notes { get; set; }
+    }
+
+    public class OrderItemResponseDto {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public string? ProductImageUrl { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+
+    public class AddressDto {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        public string AddressLine1 { get; set; } = string.Empty;
+        public string AddressLine2 { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+    }
+
+    public class PayemntDto {
+        public string PayementMethod { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public PaymentStatus Status { get; set; }
+        public DateTime? PaidAt { get; set; }
+    }
+
+    public class OrderResponseDto {
+        public Guid Id { get; set; }
+        public string OrderNumber { get; set; } = string.Empty;
+        public OrderStatus Status { get; set; }
+        public decimal TotalAmount { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal FinalAmount { get; set; }
+
+        public List<OrderItemResponseDto> Items { get; set; } = new List<OrderItemResponseDto>();
+
+        public AddressDto Address { get; set; } = null!;
+        public PayemntDto? Payemnt { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ShippedAt { get; set; }
+        public DateTime? DeliveredAt { get; set; }
+        public string? TrackingNumber { get; set; }
+
+    }
+}
