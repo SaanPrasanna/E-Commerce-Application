b16d33be2f2f204afef2ea09936a055a56794038
Sandun Prasanna <sandunpmapa@gmail.com>
2025-12-02 16:20:54 +0530

    :zap: Implement Enum Fields

--- E-CommerceApplication.Core/Entities/Order.cs
+++ E-CommerceApplication.Core/Entities/Order.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using E_CommerceApplication.Core.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
---
+++ E-CommerceApplication.Core/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_CommerceApplication.Core.Enums {
+    public enum OrderStatus {
+        Pending = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+        Cancelled = 4,
+        Returned = 5
+    }
+}
