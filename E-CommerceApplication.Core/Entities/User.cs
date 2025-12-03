337db47e73bd9cf6f65972bcce5d3e492e428e3e
Sandun Prasanna <sandunpmapa@gmail.com>
2025-12-03 23:09:06 +0530

    :zap: Update User Entity

--- E-CommerceApplication.Core/Entities/User.cs
+++ E-CommerceApplication.Core/Entities/User.cs
@@ -8,6 +8,12 @@ using Microsoft.AspNetCore.Identity;
 namespace E_CommerceApplication.Core.Entities {
     public class User : IdentityUser {
         public string? FullName { get; set; }
+        public ICollection<Address> Addresses { get; set; } = new List<Address>();
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public Cart? Cart { get; set; }
+        public Wishlist? Wishlist { get; set; }
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+
     }
 }
